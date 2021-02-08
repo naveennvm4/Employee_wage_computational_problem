6acf92cff3b07da5440b9f9d68c0610fa4cff0a0
naveennvm4 <naveennvm4@gmail.com>
2021-02-08 14:17:21 +0530

    Solving using Switch statement

--- Empwage.java
+++ Empwage.java
@@ -1,24 +1,16 @@
 public class EmpWage{
 	public static void main(String args[]) {
 		int Parttime=1;
-		int Fulltime=2;
 		int Rateperhr=20;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if(Fulltime==empCheck)
-		{
-			int Hr=16;
-			int Salary=Rateperhr*Hr;
-			System.out.println("Salary for Full Time="+Salary);
-		}
-		else if(Parttime==empCheck)
-		{
-			int Hrs=8;
-			int Salary=Rateperhr*Hrs;
-			System.out.println("Salary for Part Time="+Salary);
-		}
-		else
-		{
-			System.out.println("Salary=0");
-		}
+		switch(Parttime) 
+		      {
+		      case 1:int Hrs=8;
+  	  				int Salary1=Rateperhr*Hrs;
+  	  				System.out.println("Salary for Part Time="+Salary1);
+  	  				break;
+  	  				default: System.out.println("Salary=0");
+		    	    break;
+		      }  
 	}
 }
+
