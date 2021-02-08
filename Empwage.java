a46f568c937495ac6bef63248e32ccd0d6cc6245
naveennvm4 <naveennvm4@gmail.com>
2021-02-08 13:13:12 +0530

    calculating employee wage

--- Empwage.java
+++ Empwage.java
@@ -2,6 +2,10 @@ public class EmpWage{
 	public static void main(String args[]) {
 	int isPresent=1;
 	double empCheck= Math.floor(Math.random()*10)%2;
+	int rateperhr=20;
+	  int Hr=8;
+	  int Salary=rateperhr*Hr;
+	  System.out.println("Salary="+Salary);
 	if(empCheck == isPresent)
 		System.out.println("employee is present");
 	else
