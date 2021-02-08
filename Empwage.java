a8f31bda8d7f2b1f32509f2a67ef66a671234745
naveennvm4 <naveennvm4@gmail.com>
2021-02-08 13:28:29 +0530

    calculate Part Time employee wage

--- Empwage.java
+++ Empwage.java
@@ -1,14 +1,24 @@
 public class EmpWage{
 	public static void main(String args[]) {
-	int isPresent=1;
-	double empCheck= Math.floor(Math.random()*10)%2;
-	int rateperhr=20;
-	  int Hr=8;
-	  int Salary=rateperhr*Hr;
-	  System.out.println("Salary="+Salary);
-	if(empCheck == isPresent)
-		System.out.println("employee is present");
-	else
-		System.out.println("employee is absent");
+		int Parttime=1;
+		int Fulltime=2;
+		int Rateperhr=20;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if(Fulltime==empCheck)
+		{
+			int Hr=16;
+			int Salary=Rateperhr*Hr;
+			System.out.println("Salary for Full Time="+Salary);
+		}
+		else if(Parttime==empCheck)
+		{
+			int Hrs=8;
+			int Salary=Rateperhr*Hrs;
+			System.out.println("Salary for Part Time="+Salary);
+		}
+		else
+		{
+			System.out.println("Salary=0");
+		}
 	}
 }
