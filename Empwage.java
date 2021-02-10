8ec9cb0fee927f52b0222de2aad4a9453436e462
NaveenNK <77824596+naveennvm4@users.noreply.github.com>
2021-02-10 10:49:15 +0530

    Update Empwage.java

--- Empwage.java
+++ Empwage.java
@@ -4,12 +4,12 @@ public class EmpWage{
 		int Rateperhr=20;
 		switch(Parttime) 
 		      {
-		      case 1:int Hrs=8;
+		      case 1:	int Hrs=8;
   	  			int Salary1=Rateperhr*Hrs;
   	  			System.out.println("Salary for Part Time="+Salary1);
   	  			break;
-  	  		default: System.out.println("Salary=0");
-		    	    break;
+  	  	      default: 	System.out.println("Salary=0");
+		    	    	break;
 		      }  
 	}
 }
