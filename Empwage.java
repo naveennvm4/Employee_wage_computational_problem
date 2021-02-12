0293d3fd8af60e886b1c020bf4962a6331388918
naveennvm4 <naveennvm4@gmail.com>
2021-02-12 15:01:08 +0530

    REFACTOR: the code

--- Empwage.java
+++ Empwage.java
@@ -1,149 +1,32 @@
-import java.util.Scanner;
-public class EmpWage 
-{
-	final static int Is_Present=1;
-	final static int Rate_Per_Hr=20;
-	public void EmployeeWageUC1()
-	{
-		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		int Hr=8;
-		int Salary=Rate_Per_Hr*Hr;
-		if(Is_Present==empCheck)
-		{
-		  System.out.println("Employee is Present");
-		  System.out.println("Salary="+Salary);
+public class EmpWage{
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+	public static int computeEmpwage() {
+	int empHrs = 0, totalEmpHrs = 0,totalWorkingDays = 0;
+	while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+		totalWorkingDays++;
+		int empCheck = (int) Math.floor(Math.random() *10) % 3;
+		switch (empCheck) {
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
 		}
-		else
-		{
-		  System.out.println("Employee is Abscent");
-		  System.out.println("Salary=0");
-		  
-		}
+		totalEmpHrs += empHrs;
+		System.out.println("Daye#: "+ totalWorkingDays + "Emp HE: " +empHrs);
 	}
-	public static void main(String[] args) 
-	{
-      System.out.println("Welcometo Employee Wage Computation Program");
-      System.out.print("Enter the choice=");
-      Scanner sc=new Scanner(System.in);
-      int ch=sc.nextInt();  
-      switch(ch) 
-      {
-      case 1: 	System.out.println("To calculate the Employee Wage");
-    			EmpWage obj=new EmpWage();
-    			obj.EmployeeWageUC1();
-    	  		break;
-    	  		  
-      case 2:  	System.out.println("To calculate The part-full time Employee Wage"); 
-      			EmployeePartTime obj2=new EmployeePartTime();
-      			obj2.EmployeeUC3();
-                break;
-                   
-      case 3: 	System.out.println("To calculate Monthly employee wage");
-      			CalculateMonthWage obj3=new CalculateMonthWage();
-      			obj3.EmployeeUC5();
-      			break;
- 
-      case 4: 	System.out.println("To calulate employee wage hours ");
-      			EmployeeWageHrs obj4=new EmployeeWageHrs();
-      			obj4.employeeWageUC6();
-      			break;
-
-      default: 	System.out.println("Wrong Selection");
-      			break;
-      }  
-	}
+	int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	System.out.println ("Total Emp Wage: "+totalEmpwage);
+	return totalEmpwage;
 }
-class EmployeePartTime
-{
-	final static int Part_Time=1;
-	final static int Full_Time=2;
-	final static int Rate_Per_Hr=20;
-	public void EmployeeUC3()
-	{
-		int Hr=0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if(Full_Time==empCheck)
-		{
-			 Hr=8;
+		public static void main(String args[]) {
+			computeEmpwage();
 		}
-		else if(Part_Time==empCheck)
-		{
-			 Hr=4;
-		}
-		else
-		{
-			System.out.println("Salary=0");
-		}
-		 int Salary=Rate_Per_Hr*Hr;
-		 System.out.println("Salary="+Salary);
-	}   
 }
-class CalculateMonthWage
-{
-	final static int Part_Time=1;
-	final static int Full_Time=2;
-	final static int Rate_Per_Hr=20;
-	final static int Days_In_Month=20;
-	public void EmployeeUC5() 
-	{
-		int Hr=0;	
-		int totalEmpWage=0;
-		for(int days=0;days<Days_In_Month;days++)
-		{
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if(Full_Time==empCheck)
-		{
-			 Hr=8;
-		}
-		else if(Part_Time==empCheck)
-		{
-			 Hr=4;
-		}
-		else
-		{
-			System.out.println("Salary=0");
-		}
-		 int Salary=Rate_Per_Hr*Hr;
-		 totalEmpWage += Salary;
-		 System.out.println("Salary="+Salary);
-	  }
-		System.out.println("Total Employee Wage"+totalEmpWage);
-	}
-}
-class EmployeeWageHrs
-{
-	final static int Part_Time=4;
-	final static int Full_Time=8;
-	final static int Is_Part_Time=1;
-	final static int Is_Full_Time=2;
-	final static int Rate_Per_Hr=20;
-	final static int Max_Hr=100;
-	final static int Working_days_in_month=20;
-	public void employeeWageUC6()
-	{
-		int Hr=0;
-		int totalEmpWage=0;
-		int totalworkingday=0;
-		while(Hr < Max_Hr && totalworkingday < Working_days_in_month)
-		{
-			double empCheck = Math.floor(Math.random() * 10) % 3;
-			int check=(int)empCheck;
-			switch(check)
-			{
-			case Is_Part_Time:
-				totalEmpWage+=Rate_Per_Hr*Part_Time;
-				Hr+=Part_Time;
-				totalworkingday+=1;
-				break;
-			case Is_Full_Time:
-				totalEmpWage+=Rate_Per_Hr*Full_Time;
-				Hr+=Full_Time;
-				totalworkingday+=1;
-			default:
-			}
-		}
-		System.out.println("Hours="+Hr+"Workingdays="+totalworkingday);
-		System.out.println("Salary="+totalEmpWage);
-	}
-} 
