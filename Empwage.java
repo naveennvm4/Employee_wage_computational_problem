4a3e6de617b2ac9c9a5cd6f7e2085a075a356469
naveennvm4 <naveennvm4@gmail.com>
2021-02-19 07:44:52 +0530

    Store the daily wage along with the total Wage

--- Empwage.java
+++ Empwage.java
@@ -1,26 +1,40 @@
 import java.util.*;
 
-public class EmpWage  {
+public class EmpWage {
 	final static int IS_FULL_TIME = 1, IS_PART_TIME = 2;
 	private int numofCompany = 0;
 	private LinkedList<Main> cmpEmpWageArray;
+	private Map<String, Main> companyToEmpWage;
 
 	public EmpWage() {
 
 		cmpEmpWageArray = new LinkedList<>();
+		companyToEmpWage = new HashMap<>();
 	}
 
-	private void addCompanyEmpWage(int emp_hrs, int work_hrs, int hrs_month, String company) {
+	public void addCompanyEmpWage(int emp_hrs, int work_hrs, int hrs_month, String company) {
 		Main companyEmpWage = new Main(emp_hrs, work_hrs, hrs_month, company);
 		cmpEmpWageArray.add(companyEmpWage);
+		companyToEmpWage.put(company, companyEmpWage);
 	}
 
-	private void calcEmpWageFor() {
+	public void calcEmpWageFor() {
 		for (int i = 0; i < cmpEmpWageArray.size(); i++) {
 			Main companyEmpWage = cmpEmpWageArray.get(i);
 			companyEmpWage.setTotalEmpWage(this.calcEmpWageFor(companyEmpWage));
 			System.out.println(companyEmpWage);
 		}
+	}
+	public interface IComputeEmpWage {
+		public void addCompanyEmpWage(int EMP_RATE_PER_HR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH,
+				String company);
+
+		public void calcEmpWageFor();
+		public int getTotalWage(String company);
+	}
+
+	public int getTotalWage(String company) {
+		return companyToEmpWage.get(company).totalEmpWage;
 	}
 
 	private int calcEmpWageFor(Main m) {
@@ -39,24 +53,24 @@ public class EmpWage  {
 				empWage = 0;
 			}
 			totalEmpHrs += empHrs;
-			System.out.println("\nDAY#: " + totalWorkingDays + "EmpHr:" + empHrs);
+			System.out.println("DAY#: " + totalWorkingDays + "EmpHr:" + empHrs);
 		}
 		return totalEmpHrs = totalEmpHrs * m.EMP_RATE_PER_HR;
-	}
 
+	}
 	public class Main {
 
 		public final int EMP_RATE_PER_HR;
 		public final int NUM_OF_WORKING_DAYS;
 		public final int MAX_HRS_IN_MONTH;
-		public final String COMPANY_NAME;
+		public final String company;
 		public int totalEmpWage;
 
 		public Main(int emp_hrs, int work_hrs, int hrs_month, String company) {
 			this.EMP_RATE_PER_HR = emp_hrs;
 			this.NUM_OF_WORKING_DAYS = work_hrs;
 			this.MAX_HRS_IN_MONTH = hrs_month;
-			this.COMPANY_NAME = company;
+			this.company = company;
 		}
 
 		public void setTotalEmpWage(int totalEmpWage) {
@@ -64,16 +78,15 @@ public class EmpWage  {
 		}
 
 		public String toString() {
-			return " Total Employee Wage" + COMPANY_NAME + " : " + totalEmpWage;
+			return " Total Employee Wage" + company + " : " + totalEmpWage;
 		}
 	}
 
 	public static void main(String[] args) {
 		EmpWage obj1 = new EmpWage();
-		EmpWage obj2 = new EmpWage();
 		obj1.addCompanyEmpWage(10, 20, 30, "Dmart");
-		obj2.addCompanyEmpWage(20, 30,40, "BigBasket");
+		obj1.addCompanyEmpWage(20, 30, 40, "BigBasket");
 		obj1.calcEmpWageFor();
-		obj2.calcEmpWageFor();
+		System.out.println("Total wage for Dmart : " + obj1.getTotalWage("Dmart"));
 	}
 }
